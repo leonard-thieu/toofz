4b1633b15508e99253be9d02c02f9c93632ace0d
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-01 14:37:18 -0400

    Bump version.

--- toofz/Properties/AssemblyInfo.cs
+++ toofz/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("toofz")]
 [assembly: AssemblyProduct("toofz")]
-[assembly: AssemblyVersion("1.3.0.*")]
+[assembly: AssemblyVersion("2.0.0.*")]
 [assembly: AssemblyCompany("toofz")]
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
 [assembly: ComVisible(false)]
