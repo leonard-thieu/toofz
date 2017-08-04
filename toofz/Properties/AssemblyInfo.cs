e7ef04dbc85c51e49d52ecd1de74c1edcd1a9b65
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-04 14:27:13 -0400

    Migrate utility types from toofz-necrodancer.

--- toofz/Properties/AssemblyInfo.cs
+++ toofz/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("toofz")]
@@ -9,3 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
+
+[assembly: InternalsVisibleTo("toofz.Tests")]
---
+++ toofz/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace toofz
+{
+    public static class TypeExtensions
+    {
+        public static string GetSimpleFullName(this Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type), $"{nameof(type)} is null.");
+
+            return type.Namespace + "." + type.Name;
+        }
+    }
+}
