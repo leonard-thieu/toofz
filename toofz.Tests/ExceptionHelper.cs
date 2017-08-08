2c211eb87eca04a325b329f367e1d6e68fe06049
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-08 13:38:56 -0400

    Add support for rendering unthrown exceptions.
    The previous method for rendering exceptions did not make use of the StackTrace property. This would cause rendering to fail for custom exceptions that overrode the StackTrace property or for unthrown exceptions.

---
+++ toofz.Tests/ExceptionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using toofz.TestsShared;
+
+namespace toofz.Tests
+{
+    static class ExceptionHelper
+    {
+        static void ThrowException()
+        {
+            throw new Exception("Thrown test exception");
+        }
+
+        public static Exception GetThrownException()
+        {
+            return Record.Exception(ThrowException);
+        }
+
+    }
+}
---
+++ toofz.Tests/UnthrownException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace toofz.Tests
+{
+    sealed class UnthrownException : Exception
+    {
+        public UnthrownException(string stackTrace)
+        {
+            this.stackTrace = stackTrace;
+        }
+
+        readonly string stackTrace;
+        public override string StackTrace => stackTrace;
+    }
+}
