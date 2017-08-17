e76b4348dd8a38df6ac509cdc09ef2dec4732914
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-17 11:24:06 -0400

    Clean up AssemblyInfo.cs.

--- toofz/Properties/AssemblyInfo.cs
+++ toofz/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("toofz")]
-[assembly: AssemblyProduct("toofz")]
 [assembly: AssemblyVersion("2.4.0.*")]
-[assembly: AssemblyCompany("toofz")]
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
+
+[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
+[assembly: AssemblyProduct("toofz")]
+
+[assembly: AssemblyTitle("toofz Utilities")]
+
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("toofz.Tests")]
