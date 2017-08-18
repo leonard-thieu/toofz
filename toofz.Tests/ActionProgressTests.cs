ddf670d7640c65e643ac81d2ad7d182b70fe028b
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-17 22:57:01 -0400

    Add tests.

---
+++ toofz.Tests/ActionProgressTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using toofz.TestsShared;
+
+namespace toofz.Tests
+{
+    class ActionProgressTests
+    {
+        [TestClass]
+        public class Constructor
+        {
+            [TestMethod]
+            public void HandlerIsNull_ThrowsArgumentNullException()
+            {
+                // Arrange
+                Action<long> handler = null;
+
+                // Act
+                var ex = Record.Exception(() =>
+                {
+                    new ActionProgress<long>(handler);
+                });
+
+                // Assert
+                Assert.IsInstanceOfType(ex, typeof(ArgumentNullException));
+            }
+
+            [TestMethod]
+            public void ReturnsInstance()
+            {
+                // Arrange
+                Action<long> handler = progress => { };
+
+                // Act
+                var actionProgress = new ActionProgress<long>(handler);
+
+                // Assert
+                Assert.IsInstanceOfType(actionProgress, typeof(ActionProgress<long>));
+            }
+        }
+
+        [TestClass]
+        public class Report
+        {
+            [TestMethod]
+            public void CallsHandlerWithValue()
+            {
+                // Arrange
+                var hasBeenCalled = false;
+                var value = 20;
+                Action<long> handler = progress =>
+                {
+                    Assert.AreEqual(value, progress);
+                    hasBeenCalled = true;
+                };
+                var actionProgress = new ActionProgress<long>(handler);
+
+                // Act
+                actionProgress.Report(value);
+
+                // Assert
+                Assert.IsTrue(hasBeenCalled);
+            }
+        }
+    }
+}
