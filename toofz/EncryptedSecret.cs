c0bfc21a022b5da36135f6e3061f9a093e7b5c44
Leonard Thieu <leonard.thieu@gmail.com>
2017-09-14 18:52:29 -0400

    Remove dependency on System.ValueTuple.

--- toofz/EncryptedSecret.cs
+++ toofz/EncryptedSecret.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
@@ -8,6 +13,18 @@ namespace toofz
     [XmlRoot(EncryptedSecretName)]
     public sealed class EncryptedSecret : IXmlSerializable
     {
+        const int SaltSize = 8;
+
+        // Using the host's physical address is a compromise between security and ease of use.
+        // One of the goals of the toofz projects is for the code to be easy to evaluate and understand.
+        // Making it straight-forward to run projects is a step in this direction, however, it does mean that 
+        // we can't ask users to secure a password.
+        static readonly string Password = (from nic in NetworkInterface.GetAllNetworkInterfaces()
+                                           where nic.OperationalStatus == OperationalStatus.Up
+                                           select nic.GetPhysicalAddress())
+                                          .FirstOrDefault()
+                                          .ToString();
+
         const string EncryptedSecretName = "encryptedSecret";
         const string SecretName = "secret";
         const string SaltName = "salt";
@@ -25,11 +42,30 @@ namespace toofz
         /// </exception>
         public EncryptedSecret(string secret, int iterations)
         {
-            (this.secret, salt) = Secrets.Encrypt(secret, iterations);
+            if (string.IsNullOrEmpty(secret))
+                throw new ArgumentException($"{nameof(secret)} cannot be null or empty.", nameof(secret));
+
+            using (var pbkdf2 = new Rfc2898DeriveBytes(Password, SaltSize, iterations))
+            using (var alg = Rijndael.Create())
+            {
+                alg.Key = pbkdf2.GetBytes(32);
+                alg.IV = pbkdf2.GetBytes(16);
+
+                using (var inStream = new MemoryStream(Encoding.UTF8.GetBytes(secret)))
+                using (var outStream = new MemoryStream())
+                using (var cryptoStream = new CryptoStream(outStream, alg.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    inStream.CopyTo(cryptoStream);
+                    cryptoStream.FlushFinalBlock();
+
+                    encrypted = outStream.ToArray();
+                    salt = pbkdf2.Salt;
+                }
+            }
             this.iterations = iterations;
         }
 
-        byte[] secret;
+        byte[] encrypted;
         byte[] salt;
         int iterations;
 
@@ -39,7 +75,31 @@ namespace toofz
         /// <returns>
         /// The decrypted secret.
         /// </returns>
-        public string Decrypt() => Secrets.Decrypt(secret, salt, iterations);
+        public string Decrypt()
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(Password, salt, iterations))
+            using (var alg = Rijndael.Create())
+            {
+                alg.Key = pbkdf2.GetBytes(32);
+                alg.IV = pbkdf2.GetBytes(16);
+
+                using (var inStream = new MemoryStream(encrypted))
+                using (var outStream = new MemoryStream())
+                using (var cryptoStream = new CryptoStream(outStream, alg.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    inStream.CopyTo(cryptoStream);
+                    cryptoStream.FlushFinalBlock();
+                    outStream.Position = 0;
+
+                    using (var sr = new StreamReader(outStream))
+                    {
+                        var decrypted = sr.ReadToEnd();
+
+                        return decrypted;
+                    }
+                }
+            }
+        }
 
         #region IXmlSerializable Members
 
@@ -54,7 +114,7 @@ namespace toofz
         void IXmlSerializable.ReadXml(XmlReader reader)
         {
             reader.ReadStartElement(EncryptedSecretName);
-            secret = Convert.FromBase64String(reader.ReadElementContentAsString(SecretName, ""));
+            encrypted = Convert.FromBase64String(reader.ReadElementContentAsString(SecretName, ""));
             salt = Convert.FromBase64String(reader.ReadElementContentAsString(SaltName, ""));
             iterations = reader.ReadElementContentAsInt(IterationsName, "");
             reader.ReadEndElement();
@@ -69,7 +129,7 @@ namespace toofz
         void IXmlSerializable.WriteXml(XmlWriter writer)
         {
             writer.WriteStartElement(SecretName);
-            writer.WriteBase64(secret, 0, secret.Length);
+            writer.WriteBase64(encrypted, 0, encrypted.Length);
             writer.WriteEndElement();
 
             writer.WriteStartElement(SaltName);
