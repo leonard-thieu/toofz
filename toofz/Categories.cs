871242e8836733e345af0f17cb376071b98b0f9a
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-07 14:11:15 -0400

    Remove unnecessary APIs.

--- toofz/Categories.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Runtime.Serialization;
-
-namespace toofz
-{
-    [DataContract]
-    public sealed class Categories : Dictionary<string, Category> { }
-}
